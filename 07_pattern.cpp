35667a442b4a1187207b1fe24e68ee9e1952a168
akshatpandaofficial <akshatpandaofficial2002@gmail.com>
2021-09-22 17:13:11 +0530

    square filled with numbers
    numbers are an A.P. starting from 1 till side^2 elements

---
+++ 07_pattern.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    int k = 1;
+    while(i<n*n){
+        int j = 1;
+        while(j<=n){
+            if(k<10){
+                if(j==1){
+                    cout<<' '<<k;
+                }
+                else{
+                    cout<<"  "<<k;
+                }
+            }
+            else{
+                if(j==1){
+                    cout<<k;
+                }
+                else{
+                    cout<<' '<<k;
+                }
+            }
+            j = j + 1;
+            i = i + 1;
+            k = k + 2;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
