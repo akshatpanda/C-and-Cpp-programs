c69335af4a51ab14a0b581b7fad8e0ec0d31d461
akshatpandaofficial <akshatpandaofficial2002@gmail.com>
2021-09-23 14:37:22 +0530

    alternating binary pattern where 1 for even row coloumn sum and vice versa.

---
+++ 19_pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            if((i+j)%2==0){
+                cout<<1<<' ';
+            }
+            else{
+                cout<<0<<' ';
+            }
+            j = j + 1;
+        }
+        cout<<endl;
+        i = i + 1;
+    }
+    cout<<endl;
+    return 0;
+}
